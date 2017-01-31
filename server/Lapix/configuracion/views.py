ae92d403196f6ccb25d26f0f3575474c1510113e
darkdrei <darkdrei88@gmail.com>
2017-01-31 12:13:54 -0800

    actualizacion de los servicion list, se agrega la nueva url y se crea la clase InfoSede #61

--- server/Lapix/configuracion/views.py
+++ server/Lapix/configuracion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from configuracion.models import Sede
 from supra import views as supra
 from django.utils.decorators import method_decorator
@@ -6,33 +6,70 @@ from django.views.decorators.csrf import csrf_exempt
 import datetime
 import models
 import forms
+from django.views.generic.base import View
+from Lapix.settings import MOD_CONFIGURACION
 # Create your views here.
 
+
+class InfoSede(supra.SupraListView):
+    model = Sede
+    search_key = 'q'
+    list_display = ['nombre', 'registro', 'direccion', 'estado']
+    search_fields = ['id', 'nombre', 'direccion', 'registro']
+    paginate_by = 1
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(InfoSede, self).dispatch(request, *args, **kwargs)
+    # end def
+# end class
 
 class SedesList(supra.SupraListView):
     model = Sede
     search_key = 'q'
-    list_display = ['nombre', 'registro', 'direccion', 'estado']
+    list_display = ['nombre', 'registro', 'direccion', 'estado', 'servicios']
     search_fields = ['nombre', 'direccion', 'registro']
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(SedesList, self).dispatch(request, *args, **kwargs)
     # end def
+
+    def get_queryset(self):
+        queryset = super(SedesList , self).get_queryset()
+        self.paginate_by = self.request.GET.get('num_page', False)
+        propiedad = self.request.GET.get('sort_property', False)
+        orden = self.request.GET.get('sort_direction', False)
+        queryset2 = queryset.filter(estado=True)
+        if propiedad and orden:
+            if orden == "asc":
+                queryset2 = queryset2.order_by(propiedad)
+            elif orden == "desc":
+                propiedad = "-"+propiedad
+                queryset2 = queryset2.order_by(propiedad)
+        # end if
+        return queryset2
+    # end def
+
+    def servicios(self, obj, row):
+        edit = "/%s/edit/sede/%d/" % (MOD_CONFIGURACION, obj.id)
+        delete = "/%s/delete/sede/%d/" % (MOD_CONFIGURACION, obj.id)
+        return {'add': '/%s/add/sede/'%(MOD_CONFIGURACION), 'edit': edit, 'delete': delete}
+    # end def
 # end class
 
 class BuilderDelete():
     @staticmethod
-    def eraseModel(pk, op):
+    def eraseObjeto(pk, op):
         if op == 1:
-            obj = models.Sede.objects.filter(id=pk).first()
+            obj = models.Sede.objects.filter(id=pk, estado=True).first()
         #end if
         if obj :
             obj.estado=False
             obj.save()
-            return response([], 200)
+            return 'Ok', 200
         # end if
-        return response([], 404)
+        return 'NOT', 404
     # end def
 # end class
 
@@ -58,6 +95,15 @@ class SedeFormView(supra.SupraFormView):
     form_class = forms.SedeForm
 # end class
 
+
+class DeleteSede(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        print args,kwargs
+        respuesta, status= BuilderDelete.eraseObjeto(kwargs['pk'], 1)
+        return HttpResponse(respuesta, content_type="application/json", status=status)
+    # end def
+# end def
 
 class JornadaList(supra.SupraListView):
     model = models.Configuracion
